0ffc1c0e4aa1fb9419b8d7f8a4d220f1f721918f
Gustavo Pereira de Almeida <contato@gustavohalm.com>
2019-10-10 11:44:58 -0300

    Final

--- src/modulo.py
+++ src/modulo.py
@@ -1,2 +1,9 @@
 def func(pedras="", joia=""):
     # Escreva seu codigo aqui
+    np = 0
+    pedras_set = set(pedras)
+    for p in pedras_set:
+        for j in joia:
+            if p == j:
+                np +=1
+    return np
